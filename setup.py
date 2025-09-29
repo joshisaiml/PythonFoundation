86d7cee0d8031c2efb6db8ec66bfb04af753fb97
joshisaiml <joshis@ussilica.com>
2025-09-29 11:12:48 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements(file_path: str) -> List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+    return requirements
 
 setup(
     name="mlproject",  # Package name
@@ -16,10 +27,5 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.8",
-    install_requires=[          # Dependencies go here
-        "numpy",
-        "pandas",
-        "scikit-learn",
-        "matplotlib",
-    ],
+    install_requires=get_requirements('requirement.txt')
 )
