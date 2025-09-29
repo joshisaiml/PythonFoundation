9d4641e4cbc40fd4e60c4b22b8cd3ebf494b8314
joshisaiml <joshis@ussilica.com>
2025-09-29 11:19:59 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import find_packages, setup
 from typing import List
 
+HYPHEN_E_DOT = '-e .'
+
 def get_requirements(file_path: str) -> List[str]:
     '''
     this function will return the list of requirements
@@ -9,7 +11,12 @@ def get_requirements(file_path: str) -> List[str]:
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
         requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+            
     return requirements
+
 
 setup(
     name="mlproject",  # Package name
